3ca83b38ceaa0096637f958e866d1abd5ff30ab0
loosh <lushienlee@gmail.com>
2019-11-19 11:55:55 -0800

    Add mock output ability

--- src/main/java/tic/io/MockIO.java
+++ src/main/java/tic/io/MockIO.java
@@ -5,13 +5,20 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class MockIO extends IO {
+    public String lastOut;
 
     public MockIO(String filePath) throws IOException {
         File file = new File(filePath);
         _in = new Scanner(file);
     }
 
-//    @Override
+    @Override
+    public void showOutput(String output) {
+        lastOut = output;
+        super.showOutput(output);
+    }
+
+    //    @Override
 //    public String getInput() {
 //        return "Fake";
 //    }
